b40a745448f8dd11761a7912d64a0a1bf166c7ed
Ansh-13 <liveansh27@gmail.com>
2025-05-18 01:25:49 +0530

    some animation changes

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { forwardRef, useRef, useState } from "react";
+import React, { useRef, useState } from "react";
 import { motion, useInView } from "framer-motion";
 import CursorText from "./cursor";
 
